996354cc7d78c42a4f328f2df2425ed5097f49ee
Joaquim Viana <jviana@unilurio.ac.mz>
2025-07-26 09:03:38 -0300

    Divide in two classes CCR and BCC model

--- DEA_Solver.py
+++ DEA_Solver.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import linprog
 
 
-class DEA_CCR:
+class DEA:
+    # def __ini__(self, csv_file:pd.DataFrame, input_cols:list, output_cols:list, header:bool=True)->None:
     def __init__(self,
                  input_array:np.ndarray,
                  output_array:np.ndarray)->None:
@@ -15,6 +17,12 @@ class DEA_CCR:
         self.attr_size = self.input_size+self.output_size
         
         self.datas = np.concatenate((self.x_in, self.x_out,), axis=1)
+
+    def show_inputs(self)->pd.DataFrame:
+        pass
+
+    def show_outputs(self)->pd.DataFrame:
+        pass
 
     # Normalize datas 
     def normalize(self)->np.ndarray:
@@ -33,21 +41,30 @@ class DEA_CCR:
                     )))
         return norm_x_in, norm_x_out
 
-    # Solve the problem 
-    def solve(self)->np.ndarray:
-        norm_xi, norm_xo = self.normalize()
+        
+class DEA_CCR(DEA):
+    def __init__(self, input_array, output_array):
+        super().__init__(input_array, output_array)
 
-        lhs_ineq=np.concatenate((norm_xo*(-1), norm_xi),axis=1)
+    # Solve the problem 
+    def solve(self, normalize=True)->np.ndarray:
+        if normalize==True:
+            data_input, data_output = self.normalize()
+        else:
+            data_input, data_output = self.x_in, self.x_out
+
+        
+        lhs_ineq=np.concatenate((data_output*(-1), data_input),axis=1)
         rhs_ineq=[0]*self.DMU_size
         rhs_eq=np.array([1])
         bounds=[(0, np.inf)]*self.attr_size
 
         score=[]
         for i in range(self.DMU_size):
-            g1=norm_xi[i] #tem iterador
+            g1=data_input[i] #tem iterador
             obj=np.concatenate(([0]*self.output_size,g1),axis=0)
             
-            lhs_eq= np.concatenate((norm_xo[i], [0]*self.input_size),axis=0).reshape(1,-1)# tem iterador
+            lhs_eq= np.concatenate((data_output[i], [0]*self.input_size),axis=0).reshape(1,-1)# tem iterador
 
             res=linprog(c=obj,
                         A_ub=lhs_ineq*(-1), b_ub=rhs_ineq,
@@ -60,4 +77,16 @@ class DEA_CCR:
         score = np.array([score]).T
         score = np.round(1/score,4)
         return np.concatenate((self.x_in,self.x_out, score), axis=1)
+
+  
+
+
+class DEA_BCC(DEA):
+    def __init__(self, input_array, output_array):
+        super().__init__(input_array, output_array)
+
+# Solve the problem 
+    def solve(self, normalize=True)->np.ndarray:
         
+        return "BCC model not implemented yet in this version."
+    
